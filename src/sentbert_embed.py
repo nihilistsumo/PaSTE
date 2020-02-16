875ddda681576f3a1b682dd437fe3965a539d1ba
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-02-16 14:13:15 -0500

    add para embed code

---
+++ src/sentbert_embed.py
@@ -0,0 +1,51 @@
+from sentence_transformers import SentenceTransformer
+import argparse
+import numpy as np
+
+# get embeddings of the whole para
+def get_embeddings(paratext_file, model_name, outdir, saveid=False, batch_size=10000):
+    model = SentenceTransformer(model_name)
+    print("Using "+model_name+" to embed paras as a whole")
+    with open(paratext_file, 'r') as pt:
+        c = 0
+        part = 0
+        texts = []
+        ids = []
+        for l in pt:
+            ids.append(l.split('\t')[0])
+            texts.append(l.split('\t')[1])
+            c += 1
+            if c % 10000 == 0:
+                print("Going to embed")
+                embeds = model.encode(texts)
+                part += 1
+                print("Embedding complete, going to save part "+str(part)+", "+str(c)+" paras embedded\n")
+                np.save(outdir + '/' + model_name + '-part' + str(part), embeds)
+                texts = []
+        print("Going to embed")
+        embeds = model.encode(texts)
+        part += 1
+        print("Embedding complete, going to save part " + str(part) + ", " + str(c) + " paras embedded\n")
+        np.save(outdir + '/' + model_name + '-part' + str(part), embeds)
+        if saveid:
+            print("Saving paraids file")
+            ids = np.array(ids)
+            np.save(outdir + '/paraids', ids)
+
+def main():
+    parser = argparse.ArgumentParser(description='Use sentence-transformers to embed paragraphs')
+    parser.add_argument('-p', '--paratext_file', help='Path to paratext file')
+    parser.add_argument('-m', '--model_name', help='Name of the model to be used for embedding')
+    parser.add_argument('-o', '--outdir', help='Path to output dir')
+    parser.add_argument('-i', '--save_id', type=bool, default=False, help='Save id file?')
+    parser.add_argument('-b', '--batch_size', default=10000, help='Size of each batch to be encoded')
+    args = vars(parser.parse_args())
+    paratext_file = args['paratext_file']
+    model_name = args['model_name']
+    outdir = args['outdir']
+    saveid = args['save_id']
+    batch = int(args['batch_size'])
+    get_embeddings(paratext_file, model_name, outdir, saveid, batch)
+
+if __name__ == '__main__':
+    main()
