5a1a8580fcc658e354e7371068d19e76c01d0748
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-02-22 23:40:34 -0500

    scaled variation implement

--- src/query_attn_network.py
+++ src/query_attn_network.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from sentence_transformers import SentenceTransformer
 import numpy as np
@@ -10,7 +11,6 @@ class Neural_Network(nn.Module):
     def __init__(self, ):
         super(Neural_Network, self).__init__()
         # parameters
-        # TODO: parameters can be parameterized instead of declaring them here
         self.emb_size = 768
         self.cosine_sim = nn.CosineSimilarity()
         self.LL1 = nn.Linear(self.emb_size, self.emb_size*self.emb_size)
@@ -23,6 +23,36 @@ class Neural_Network(nn.Module):
         self.z = self.z.view(-1, self.emb_size, self.emb_size)
         self.sXp1 = torch.matmul(self.Xp1.view(-1, 1, self.emb_size), self.z).view(-1, self.emb_size)
         self.sXp2 = torch.matmul(self.Xp2.view(-1, 1, self.emb_size), self.z).view(-1, self.emb_size)
+        o = self.cosine_sim(self.sXp1, self.sXp2)  # final activation function
+        return o
+
+    def num_flat_features(self, X):
+        size = X.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+    def predict(self, X_test):
+        print("Predicted data based on trained weights: ")
+        print("Input (scaled): \n" + str(X_test))
+        print("Output: " + str(self.forward(X_test)))
+
+class Neural_Network_scale(nn.Module):
+    def __init__(self, ):
+        super(Neural_Network_scale, self).__init__()
+        # parameters
+        self.emb_size = 768
+        self.cosine_sim = nn.CosineSimilarity()
+        self.LL1 = nn.Linear(self.emb_size, self.emb_size)
+
+    def forward(self, X):
+        self.Xq = X[:, :self.emb_size]
+        self.Xp1 = X[:, self.emb_size:2*self.emb_size]
+        self.Xp2 = X[:, 2*self.emb_size:]
+        self.z = torch.relu(self.LL1(self.Xq))
+        self.sXp1 = F.normalize(torch.mul(self.Xp1, self.z), p=2, dim=1)
+        self.sXp2 = F.normalize(torch.mul(self.Xp2, self.z), p=2, dim=1)
         o = self.cosine_sim(self.sXp1, self.sXp2)  # final activation function
         return o
 
@@ -89,10 +119,6 @@ def get_data(emb_dir, emb_model_name, query_attn_data_file):
     return (torch.tensor(X_train), torch.tensor(y_train))
 
 def main():
-    cosine_sim = nn.CosineSimilarity()
-    NN = Neural_Network()
-    criterion = nn.MSELoss()
-    opt = optim.SGD(NN.parameters(), lr=0.01)
     # X = torch.tensor(torch.randn(256, 12))
     # target = torch.tensor(torch.tensor(([0.1, 0, 0, 0.4], [0, 0.3, 0.2, 0.1], [0.1, 0.2, 0.2, 0.1], [0, 0.8, 0.1, 0.1])))
     #
@@ -107,23 +133,40 @@ def main():
     # X_testp2 = torch.matmul(X_test[:, 8:], torch.t(X_testq))
     # y_test = cosine_sim(X_testp1, X_testp2)
 
-    parser = argparse.ArgumentParser(description="Train and evaluate query attentive network for paragraph similarity task")
+    parser = argparse.ArgumentParser(description='Train and evaluate query attentive network for paragraph similarity task')
     parser.add_argument('-e', '--emb_dir', help='Path to para embedding directory')
     parser.add_argument('-et', '--emb_dir_test', help='Path to para embedding directory for test split paras')
+    parser.add_argument('-n', '--neural_model', help='Neural model variation (1/2)')
+    parser.add_argument('-lr', '--learning_rate', help='Learning rate')
     parser.add_argument('-m', '--emb_model_name', help='Name of the model used to embed the paras')
     parser.add_argument('-d', '--train_data_file', help='Path to train data file')
     parser.add_argument('-t', '--test_data_file', help='Path to test data file')
+    parser.add_argument('-o', '--model_outfile', help='Path to save the trained model')
     args = vars(parser.parse_args())
     emb_dir = args['emb_dir']
     emb_dir_test = args['emb_dir_test']
+    variation = int(args['neural_model'])
+    lrate = float(args['learning_rate'])
     model_name = args['emb_model_name']
     train_filepath = args['train_data_file']
     test_filepath = args['test_data_file']
+    model_out = args['model_outfile']
     X, y = get_data(emb_dir, model_name, train_filepath)
     if emb_dir_test == '':
         X_test, y_test = get_data(emb_dir, model_name, test_filepath)
     else:
         X_test, y_test = get_data(emb_dir_test, model_name, test_filepath)
+
+    cosine_sim = nn.CosineSimilarity()
+    if variation == 1:
+        NN = Neural_Network()
+    elif variation == 2:
+        NN = Neural_Network_scale()
+    else:
+        print('Wrong model variation selected!')
+        exit(1)
+    criterion = nn.MSELoss()
+    opt = optim.SGD(NN.parameters(), lr=lrate)
     for i in range(1000):  # trains the NN 1,000 times
         opt.zero_grad()
         output = NN(X)
@@ -134,8 +177,10 @@ def main():
     # NN.saveWeights(NN)
     NN.predict(X_test)
     print(NN.parameters())
-    print("True output: " + str(y_test))
+    print('True output: ' + str(y_test))
     print('Features: ' + str(NN.num_flat_features(X_test)))
+    print('Saving model at ' + model_out)
+    torch.save(NN.state_dict(), model_out)
 
 if __name__ == '__main__':
     main()
