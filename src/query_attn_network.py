b2fe67c539a911162532fcc30cbf4098537c42bd
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-02-26 13:50:13 -0500

    fix forward dummy model

--- src/query_attn_network.py
+++ src/query_attn_network.py
@@ -286,7 +286,7 @@ def main():
         NN = Neural_Network_scale()
     elif variation == 0:
         NN = Dummy_cosine_sim()
-        y_pred = NN.predict(X_test).numpy()
+        y_pred = NN.forward(X_test)
         auc_score = roc_auc_score(y_test, y_pred)
         print('AUC score: ' + str(auc_score))
         sys.exit(0)
