310ea0eefcfa2c19b0b1cd3bedddc93a383e3d9c
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-02-26 11:49:14 -0500

    no norm

--- src/query_attn_network.py
+++ src/query_attn_network.py
@@ -50,13 +50,43 @@ class Neural_Network_scale(nn.Module):
         self.LL1 = nn.Linear(self.emb_size, self.emb_size)
 
     def forward(self, X):
-        self.Xq = F.normalize(X[:, :self.emb_size], p=2, dim=1)
-        self.Xp1 = F.normalize(X[:, self.emb_size:2*self.emb_size], p=2, dim=1)
-        self.Xp2 = F.normalize(X[:, 2*self.emb_size:], p=2, dim=1)
+        self.Xq = X[:, :self.emb_size]
+        self.Xp1 = X[:, self.emb_size:2*self.emb_size]
+        self.Xp2 = X[:, 2*self.emb_size:]
         self.z = torch.relu(self.LL1(self.Xq))
-        self.sXp1 = F.normalize(torch.mul(self.Xp1, self.z), p=2, dim=1)
-        self.sXp2 = F.normalize(torch.mul(self.Xp2, self.z), p=2, dim=1)
+        self.sXp1 = torch.mul(self.Xp1, self.z)
+        self.sXp2 = torch.mul(self.Xp2, self.z)
         o = self.cosine_sim(self.sXp1, self.sXp2)  # final activation function
+        return o
+
+    def num_flat_features(self, X):
+        size = X.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+    def predict(self, X_test):
+        print("Predicted data based on trained weights: ")
+        print("Input (scaled): \n" + str(X_test))
+        y_pred = self.forward(X_test)
+        print("Output: " + str(y_pred))
+        return y_pred
+
+class Neural_Network_siamese(nn.Module):
+    def __init__(self, ):
+        super(Neural_Network_siamese, self).__init__()
+        # parameters
+        self.emb_size = 768
+        self.cosine_sim = nn.CosineSimilarity()
+        self.LL1 = nn.Linear(self.emb_size, self.emb_size)
+
+    def forward(self, X):
+        self.Xp1 = X[:, :self.emb_size]
+        self.Xp2 = X[:, self.emb_size:]
+        self.z1 = torch.relu(self.LL1(self.Xp1))
+        self.z2 = torch.relu(self.LL1(self.Xp2))
+        o = self.cosine_sim(self.z1, self.z2)
         return o
 
     def num_flat_features(self, X):
