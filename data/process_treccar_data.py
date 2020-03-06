e674e7e78425f27559bc3dac47e373ca864c0b83
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-03-06 13:29:54 -0500

    fix trec data process

--- data/process_treccar_data.py
+++ data/process_treccar_data.py
@@ -1,7 +1,9 @@
 from trec_car.read_data import *
 import sys
+import re
 
 def write_paratext_data(para_cbor, outfile):
+    _RE_COMBINE_WHITESPACE = re.compile(r"\s+")
     c = 0
     with open(outfile, 'w') as out:
         with open(para_cbor, 'rb') as f:
@@ -10,6 +12,7 @@ def write_paratext_data(para_cbor, outfile):
                          else elem.anchor_text
                          for elem in p.bodies]
                 text = ' '.join(texts)
+                text = _RE_COMBINE_WHITESPACE.sub(" ", text).strip()
                 out.write(p.para_id + '\t' + text + '\n')
                 c += 1
                 if c % 10000 == 0:
