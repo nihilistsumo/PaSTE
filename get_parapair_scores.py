61ae46dfbc972668e94eec9ef553000e22c93b3f
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-04-11 23:05:36 -0400

    back to normal float conv

--- get_parapair_scores.py
+++ get_parapair_scores.py
@@ -19,7 +19,7 @@ def write_parapair_scores(nn_model_path, emb_model_name, emb_vec_file, emb_pids_
             parapairs.append(l.split('\t')[2]+'_'+l.split('\t')[3].rstrip())
     parapair_score_dict = {}
     for i in range(len(parapairs)):
-        parapair_score_dict[parapairs[i]] = float(json.dumps(y_pred[i]))
+        parapair_score_dict[parapairs[i]] = float(y_pred[i])
         if i%10000 == 0:
             print(str(i) + ' samples inferred')
     with open(outfile, 'w') as out:
